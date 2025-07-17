32faa68decfa7a9128baff522deead3853534a41
James Braza <jamesbraza@gmail.com>
2025-07-17 13:01:02 -0700

    Renamed 'aviary.litqa' to 'aviary.labbench'

--- packages/labbench/tests/test_litqa_env.py
+++ packages/labbench/tests/test_litqa_env.py
@@ -26,7 +26,7 @@ from paperqa.agents.tools import Complete, GatherEvidence, GenerateAnswer
 from paperqa.prompts import CANNOT_ANSWER_PHRASE
 from pytest_subtests import SubTests
 
-from aviary.envs.litqa import (
+from aviary.envs.labbench import (
     DEFAULT_REWARD_MAPPING,
     GradablePaperQAEnvironment,
     LitQATaskDataset,
@@ -145,7 +145,7 @@ class StubLitQADataset(LitQATaskDataset):
         return len(self.data)
 
 
-STUB_TASK_DATASET_NAME = "stub-litqa"
+STUB_TASK_DATASET_NAME = "stub-labbench"
 TASK_DATASET_REGISTRY[STUB_TASK_DATASET_NAME] = (
     StubLitQADataset.__module__,
     StubLitQADataset.__name__,
