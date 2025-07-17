e89022352ac2ad2a59e98ca69fdca587d160fc87
James Braza <jamesbraza@gmail.com>
2025-07-17 13:01:02 -0700

    Renamed 'aviary.litqa' to 'aviary.labbench'

--- packages/labbench/src/aviary/envs/labbench/task.py
+++ packages/labbench/src/aviary/envs/labbench/task.py
@@ -28,7 +28,7 @@ from paperqa.docs import Docs
 from paperqa.settings import Settings
 from paperqa.types import DocDetails, PQASession
 
-from aviary.envs.litqa import (
+from aviary.envs.labbench import (
     DEFAULT_REWARD_MAPPING,
     GradablePaperQAEnvironment,
 )
@@ -191,7 +191,7 @@ def read_litqa_v2_from_hub(
     except ImportError as exc:
         raise ImportError(
             "Reading in LitQA2 requires the 'datasets' extra for 'datasets'. Please:"
-            " `pip install aviary.litqa[datasets]`."
+            " `pip install aviary.labbench[datasets]`."
         ) from exc
 
     train_eval = load_dataset(train_eval_dataset, "LitQA2")["train"].to_pandas()
