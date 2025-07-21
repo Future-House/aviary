0de54a2cfb0fd372367a9c2b5a99658b4e3e56a5
James Braza <jamesbraza@gmail.com>
2025-07-20 23:12:57 -0700

    Added support for TableQA and FigQA, both PDF and image variants, and updated LFRQA to match, with tests

--- packages/labbench/src/aviary/envs/labbench/__init__.py
+++ packages/labbench/src/aviary/envs/labbench/__init__.py
@@ -1,27 +1,31 @@
 from .env import (
     DEFAULT_REWARD_MAPPING,
     GradablePaperQAEnvironment,
+    ImageQAEnvironment,
     make_discounted_returns,
 )
 from .task import (
     DEFAULT_AVIARY_PAPER_HF_HUB_NAME,
     DEFAULT_LABBENCH_HF_HUB_NAME,
-    TASK_DATASET_NAME,
-    LitQATaskDataset,
-    LitQAv2TaskDataset,
-    LitQAv2TaskSplit,
-    read_litqa_v2_from_hub,
+    ImageQATaskDataset,
+    LABBenchDatasets,
+    PaperQATaskDataset,
+    TextQATaskDataset,
+    TextQATaskSplit,
+    read_ds_from_hub,
 )
 
 __all__ = [
     "DEFAULT_AVIARY_PAPER_HF_HUB_NAME",
     "DEFAULT_LABBENCH_HF_HUB_NAME",
     "DEFAULT_REWARD_MAPPING",
-    "TASK_DATASET_NAME",
     "GradablePaperQAEnvironment",
-    "LitQATaskDataset",
-    "LitQAv2TaskDataset",
-    "LitQAv2TaskSplit",
+    "ImageQAEnvironment",
+    "ImageQATaskDataset",
+    "LABBenchDatasets",
+    "PaperQATaskDataset",
+    "TextQATaskDataset",
+    "TextQATaskSplit",
     "make_discounted_returns",
-    "read_litqa_v2_from_hub",
+    "read_ds_from_hub",
 ]
